3aac672a877291cd8e09b0f54acadb0f168884ec
simonbeirouti <hello@simonbeirouti.com>
2024-11-01 00:11:27 +0700

    add env

--- packages/mintbase/src/bitte-provider.tsx
+++ packages/mintbase/src/bitte-provider.tsx
@@ -4,11 +4,12 @@ import { BitteWalletContextProvider } from "@mintbase-js/react";
 
 const BitteWalletSetup = {
   contractAddress:
-    process.env.NEXT_PUBLIC_CONTRACT_ADDRESS ||
-    "hellovirtualworld.mintspace2.testnet",
+    process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || 
+    "akjsfhkajhsdasd.testnet",
   network: process.env.NEXT_PUBLIC_NETWORK || "testnet",
   callbackUrl:
-    process.env.NEXT_PUBLIC_CALLBACK_URL ||
+    process.env.NEXT_PUBLIC_CALLBACK_URL_WEB ||
+    process.env.NEXT_PUBLIC_CALLBACK_URL_DOCS ||
     (typeof window !== "undefined" ? window.location.origin : ""),
 };
 
