5aecdbb282fdb5871df12a3125b6014568e3765c
simonbeirouti <hello@simonbeirouti.com>
2024-11-01 02:53:53 +0700

    anudda 1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'standalone',
+  distDir: '.next',
+  transpilePackages: ["@repo/ui", "@repo/theme", "@repo/near", "@repo/mintbase", "@repo/icons"],
+  experimental: {
+    externalDir: true
+  }
+}
 
 module.exports = nextConfig 
